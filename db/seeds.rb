421690d886ce4a2a9d61e7faa75acc38d068f02a
Gabriel Packer <ggkpacker@gmail.com>
2018-05-03 14:19:52 -0300

    fix identation

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ User.destroy_all
 Wallet.destroy_all
 
 puts "Creating users and wallets"
-  CSV.foreach("lib/resources/wallets.csv", headers: true) do |row|
-    User.create(name: row[0])
-    user = User.find_by(name: row[0])
-    Wallet.create!(currency: row[1], amount: row[2].to_i, user_id: user.id)
-  end
+CSV.foreach("lib/resources/wallets.csv", headers: true) do |row|
+  User.create(name: row[0])
+  user = User.find_by(name: row[0])
+  Wallet.create!(currency: row[1], amount: row[2].to_i, user_id: user.id)
+end
 puts "created #{User.count} users and #{Wallet.count} wallets"
 puts "Done"
