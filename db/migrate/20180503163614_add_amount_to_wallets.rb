3cd672174d94fb2274f0532c5fa308ed1087a1ed
Gabriel Packer <ggkpacker@gmail.com>
2018-05-03 14:40:43 -0300

    change bigint to bigdecimal

--- db/migrate/20180503163614_add_amount_to_wallets.rb
+++ db/migrate/20180503163614_add_amount_to_wallets.rb
@@ -1,5 +1,5 @@
 class AddAmountToWallets < ActiveRecord::Migration[5.1]
   def change
-    add_column :wallets, :amount, :integer, limit: 8
+    add_column :wallets, :amount, :decimal, precision: 8, scale: 2
   end
 end
