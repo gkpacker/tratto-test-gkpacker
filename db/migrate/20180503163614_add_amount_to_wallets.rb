8d5602e7e94dfe33aea4e222953248b825c75c2a
Gabriel Packer <ggkpacker@gmail.com>
2018-05-03 21:51:16 -0300

    add default 0 to wallet

--- db/migrate/20180503163614_add_amount_to_wallets.rb
+++ db/migrate/20180503163614_add_amount_to_wallets.rb
@@ -1,5 +1,5 @@
 class AddAmountToWallets < ActiveRecord::Migration[5.1]
   def change
-    add_column :wallets, :amount_cents, :decimal, precision: 8, scale: 2
+    add_column :wallets, :amount_cents, :decimal, precision: 8, scale: 2, default: 0
   end
 end
